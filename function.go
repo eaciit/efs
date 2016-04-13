50ed9e54f3c733dc0c37d75f406c7c29fa9ef31a
alipsidikp <alip.sidik.p@gmail.com>
2016-04-14 06:21:57 +0700

    Completed sum function,

--- function.go
+++ function.go
@@ -3,13 +3,51 @@ package efs
 import (
 	// "errors"
 	"github.com/eaciit/toolkit"
-	// "strings"
+	"strings"
 )
 
 type Fn func(tkm toolkit.M, formula string) interface{}
 type MapFn map[string]Fn
 
 var efsfunc MapFn
+
+var fnSUM Fn = func(tkm toolkit.M, formula string) interface{} {
+	var fval float64
+	// Process in here : formula SUM(@3..@5)
+	acval := strings.Split(string(formula[4:len(formula)-1]), ",")
+	for _, cval := range acval {
+		if strings.Contains(cval, "..") {
+			dotcval := strings.Split(cval, "..")
+			i1 := toolkit.ToInt(dotcval[0][1:], toolkit.RoundingAuto)
+			i2 := toolkit.ToInt(dotcval[1][1:], toolkit.RoundingAuto)
+			cval = ""
+			for i := i1; i <= i2; i++ {
+				if len(cval) > 0 {
+					cval += string(",")
+				}
+				cval += toolkit.Sprintf("@%v", i)
+			}
+		}
+		// toolkit.Printf("DEBUG 31 : %v\n", cval)
+		tacval := strings.Split(cval, ",")
+		for _, tcval := range tacval {
+			fval += toolkit.ToFloat64(tkm.Get(tcval, 0), 6, toolkit.RoundingAuto)
+		}
+	}
+	return fval
+}
+
+var fnIF Fn = func(tkm toolkit.M, formula string) interface{} {
+	var fval float64
+	// Process in here
+	return fval
+}
+
+var fnAVG Fn = func(tkm toolkit.M, formula string) interface{} {
+	var fval float64
+	// Process in here
+	return fval
+}
 
 func (m MapFn) Addfunction(name string, fn Fn) MapFn {
 	m[name] = fn
@@ -24,18 +62,7 @@ func (m MapFn) Has(name string) bool {
 func init() {
 	efsfunc = make(MapFn, 0)
 
-	efsfunc.Addfunction("SUM", func(tkm toolkit.M, formula string) interface{} {
-		var fval float64
-		return fval
-	})
-
-	efsfunc.Addfunction("IF", func(tkm toolkit.M, formula string) interface{} {
-		var fval float64
-		return fval
-	})
-
-	efsfunc.Addfunction("AVG", func(tkm toolkit.M, formula string) interface{} {
-		var fval float64
-		return fval
-	})
+	efsfunc.Addfunction("SUM", fnSUM)
+	efsfunc.Addfunction("IF", fnIF)
+	efsfunc.Addfunction("AVG", fnAVG)
 }
