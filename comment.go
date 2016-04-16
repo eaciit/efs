145ae5505f78c5d19121f95e95c823941e9b9532
alipsidikp <alip.sidik.p@gmail.com>
2016-04-16 12:50:55 +0700

    #

--- comment.go
+++ comment.go
@@ -3,7 +3,7 @@ package efs
 import (
 	"github.com/eaciit/dbox"
 	"github.com/eaciit/orm/v1"
-	"github.com/eaciit/toolkit"
+	// "github.com/eaciit/toolkit"
 )
 
 type Comments struct {
@@ -35,8 +35,8 @@ func Countcomment(sveid string) int {
 	return n
 }
 
-func Getcomment(sveid string) (arrstr []string) {
-	arrstr = make([]string, 0, 0)
+func Getcomment(sveid string) (arrcom []Comments) {
+	arrcom = make([]Comments, 0, 0)
 	c := new(Comments)
 
 	filter := dbox.Eq("sveid", sveid)
@@ -46,11 +46,11 @@ func Getcomment(sveid string) (arrstr []string) {
 		return
 	}
 
-	artkm := make([]toolkit.M, 0, 0)
-	err = cur.Fetch(&artkm, 0, false)
-	for _, val := range artkm {
-		arrstr = append(arrstr, toolkit.ToString(val.Get("text", "")))
-	}
+	// artkm := make([]toolkit.M, 0, 0)
+	err = cur.Fetch(&arrcom, 0, false)
+	// for _, val := range artkm {
+	// 	arrstr = append(arrstr, toolkit.ToString(val.Get("text", "")))
+	// }
 
 	return
 }
