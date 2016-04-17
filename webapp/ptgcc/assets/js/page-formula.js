fbb3ce1c7418a4a4be2379bd226ad613e2195603
Arfian Bagus <arfianbagus@Arfians-MacBook-Pro.local>
2016-04-17 19:05:16 +0700

    no message

--- webapp/ptgcc/assets/js/page-formula.js
+++ webapp/ptgcc/assets/js/page-formula.js
@@ -667,8 +667,7 @@ fp.removeColumnFormula = function(index){
         dataStatement.Element[i].ElementVersion.splice(aa,1);
     }
     ko.mapping.fromJS(dataStatement, fp.dataFormula);
-    aa = parseInt(index)+1;
-    $(".table-formula-data>thead>tr.searchsv td[indexid="+aa+"]").remove();
+    $(".table-formula-data>thead>tr.searchsv td[indexid="+index+"]").remove();
     fp.refreshHeightTable();
 };
 fp.selectSimulate = function(index){
