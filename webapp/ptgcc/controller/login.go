15137de2d456f13d786bfe9c0445808480f85fcb
Rino Sukmandityo <rino.sukmandityo@gmail.com>
2016-05-18 19:17:35 +0700

    add acl for login
    add acl for login

--- webapp/ptgcc/controller/login.go
+++ webapp/ptgcc/controller/login.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/eaciit/efs/webapp/ptgcc/helper"
+	"github.com/eaciit/efs/webapp/ptgcc/model"
 	"github.com/eaciit/knot/knot.v1"
 	"github.com/eaciit/toolkit"
 )
@@ -16,34 +17,73 @@ func CreateLoginController(s *knot.Server) *LoginController {
 	return controller
 }
 
-func (s *LoginController) ProcessLogin(r *knot.WebContext) interface{} {
+func (l *LoginController) ProcessLogin(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputJson
+
+	payload := toolkit.M{}
+	if err := r.GetPayload(&payload); err != nil {
+		return helper.CreateResult(false, "", err.Error())
+	}
+
+	sessid, err := new(efscore.Login).LoginProcess(payload)
+	if err != nil {
+		return helper.CreateResult(false, "", err.Error())
+	}
+	r.SetSession("sessionid", sessid)
+
+	return helper.CreateResult(true, toolkit.M{}.Set("status", true).Set("sessionid", sessid), "Login success")
+}
+
+func (l *LoginController) Logout(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputJson
+	r.SetSession("sessionid", "")
+	return helper.CreateResult(true, nil, "Logout success")
+}
+
+func (l *LoginController) ResetPassword(r *knot.WebContext) interface{} {
 	r.Config.OutputType = knot.OutputJson
 
 	payload := toolkit.M{}
 	err := r.GetPayload(&payload)
-
-	switch {
-	case err != nil:
+	if err != nil {
 		return helper.CreateResult(false, nil, err.Error())
-	case !payload.Has("username") || !payload.Has("password"):
-		return helper.CreateResult(false, nil, "username or password not found")
-	case payload.Has("username") && len(toolkit.ToString(payload["username"])) == 0:
-		return helper.CreateResult(false, nil, "username cannot empty")
-	case payload.Has("password") && len(toolkit.ToString(payload["password"])) == 0:
-		return helper.CreateResult(false, nil, "password cannot empty")
+	}
+	if err = new(efscore.Login).ResetPassword(payload); err != nil {
+		return helper.CreateResult(false, nil, err.Error())
 	}
 
-	if toolkit.ToString(payload["username"]) == "eaciit" && toolkit.ToString(payload["password"]) == "Password.1" {
-		sesid := toolkit.RandomString(32)
-		r.SetSession("sessionid", sesid)
-		return helper.CreateResult(true, toolkit.M{}.Set("status", true).Set("sessionid", sesid), "Login Success")
-	}
-
-	return helper.CreateResult(true, "", "Login failed")
+	return helper.CreateResult(true, nil, "reset password success")
 }
 
-func (s *LoginController) Logout(r *knot.WebContext) interface{} {
+func (l *LoginController) SavePassword(r *knot.WebContext) interface{} {
 	r.Config.OutputType = knot.OutputJson
-	r.SetSession("sessionid", "")
-	return helper.CreateResult(true, nil, "Logout success")
+
+	payload := toolkit.M{}
+	err := r.GetPayload(&payload)
+	if err != nil {
+		return helper.CreateResult(false, nil, err.Error())
+	}
+
+	if err = new(efscore.Login).SavePassword(payload); err != nil {
+		return helper.CreateResult(false, nil, err.Error())
+	}
+
+	return helper.CreateResult(true, nil, "save password success")
+}
+
+func (l *LoginController) Authenticate(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputJson
+
+	payload := toolkit.M{}
+
+	if err := r.GetPayload(&payload); err != nil {
+		return helper.CreateResult(false, nil, err.Error())
+	}
+
+	result, err := new(efscore.Login).Authenticate(payload)
+	if err != nil {
+		return helper.CreateResult(false, nil, err.Error())
+	}
+
+	return helper.CreateResult(true, result, "")
 }
